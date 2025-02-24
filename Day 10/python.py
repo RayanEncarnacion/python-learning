ae44fbf2f4f5a4c12038935b6998ac99fc3d5b60
Rayan Encarnacion <rayanbalbuena@gmail.com>
2025-02-24 18:57:35 -0400

    Added types to fn args, constant in uppercase and passed result to continue_calculations fn

--- Day 10/python.py
+++ Day 10/python.py
@@ -1,23 +1,23 @@
 import operator
 
-allowed_operators = {
+ALLOWED_OPERATORS = {
     "+": operator.add,
     "-": operator.sub,
     "*": operator.mul,
     "/": operator.truediv
 }
 
-def continue_calculations() -> bool:
+def continue_calculations(result: float) -> bool:
     return input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ").lower() == 'y'
 
-def calculate(first_number: int | None, operator, second_number):
-    return allowed_operators[operator](first_number, second_number)
+def calculate(first_number: int | None, operator: str, second_number: int) -> int | float:
+    return ALLOWED_OPERATORS[operator](first_number, second_number)
 
-def complete_calculation(first_number) -> int | float:
-    for item in allowed_operators.keys():
-        print(item + '\n')
+def complete_calculation(first_number: int | float) -> int | float:
+    for op in ALLOWED_OPERATORS:
+        print(op)
         
-    operator = input("Pick an operator from the above: ")
+    operator = input("Pick an operator from the above: ").strip()
     next_number = int(input("What's the next number?: "))
     result = calculate(first_number, operator, next_number)
     print(f'{first_number} {operator} {next_number} = {result}')
@@ -26,5 +26,5 @@ def complete_calculation(first_number) -> int | float:
 first_number = int(input("What's the first number?: "))
 result = complete_calculation(first_number)
 
-while continue_calculations():
+while continue_calculations(result):
     complete_calculation(result)
